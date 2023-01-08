fcc1f342c38cdb6f8677fa110a8c77db06e4aee6
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-01-08 12:00:01 -0500

    Add files via upload

---
+++ usb.js
@@ -0,0 +1,29 @@
+async function doWebAuthnCreate() {
+    const publicKeyCredentialCreationOptions = {
+        challenge: Uint8Array.from(
+            "hi", c => c.charCodeAt(0)),
+        rp: {
+            name: "AuthSheild™",
+            id: "webauthn.madebyesmel.com",
+        },
+        user: {
+            id: Uint8Array.from(
+                "UZSL85T9AFC", c => c.charCodeAt(0)),
+            name: "0001",
+            displayName: "Andrew Frykman",
+        },
+        pubKeyCredParams: [{alg: -7, type: "public-key"}],
+        authenticatorSelection: {
+            authenticatorAttachment: "platform",
+        },
+        timeout: 60000,
+        attestation: "none"
+    };
+    
+    const credential = await navigator.credentials.create({
+        publicKey: publicKeyCredentialCreationOptions
+    });
+
+    console.log(credential);
+    
+}
