ded97ebfbe26bac0ae50b427d28b4a4e0856ff48
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-01-14 18:01:01 -0500

    d

--- usb.js
+++ usb.js
@@ -12,39 +12,40 @@ async function doWebAuthnCreate() {
             name: "Andrew Frykman",
             displayName: "Andrew Frykman",
         },
-        pubKeyCredParams: [{alg: -7, type: "public-key"}],
+        pubKeyCredParams: [{ alg: -7, type: "public-key" }],
         authenticatorSelection: {
             authenticatorAttachment: "cross-platform",
         },
         timeout: 60000,
         attestation: "none"
     };
-    
+
     const credential = await navigator.credentials.create({
         publicKey: publicKeyCredentialCreationOptions
     });
 
     console.log(credential);
-    
+
+    const input = document.getElementById("passkey-input").value;
+    const crypto = window.crypto || window.msCrypto;
+    const hash = crypto.subtle.digest("SHA-256", new TextEncoder().encode(credential));
+
+    const hashArray = new Uint8Array(hash);
+    let hashedInput = '';
+    hashArray.forEach(b => hashedInput += b.toString(16).padStart(2, '0'));
+
+    const storedPasskey = localStorage.getItem("hashedPasskey");
+
+    if (hashedInput === storedPasskey) {
+        alert('Authenticated');
+    } else {
+        document.location.href = "https://apple.com";
+    }
+
+
 }
 
 // if (credential === 'lvtEMttziB28TVipHdmL1wjXfHJN9GSVLYREaf10bvo') {
 //     window.location.replace("https://www.apple.com/");
 // }
 
-// const input = document.getElementById("passkey-input").value;
-const crypto = window.crypto || window.msCrypto;
-const hash = crypto.subtle.digest("SHA-256", new TextEncoder().encode(credential));
-
-const hashArray = new Uint8Array(hash);
-let hashedInput = '';
-hashArray.forEach(b => hashedInput += b.toString(16).padStart(2, '0'));
-
-const storedPasskey = localStorage.getItem("hashedPasskey");
-
-if (hashedInput === storedPasskey) {
-    alert('Authenticated');
-  } else {
-    document.location.href = "https://apple.com";
-  }
-  
